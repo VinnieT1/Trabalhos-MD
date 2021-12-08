e4e6c9f69edac440de4318da90474cb0043aaad9
VinnieT1 <92376175+VinnieT1@users.noreply.github.com>
2021-12-08 11:58:34 -0300

    Update Questao_7.c

--- Questões Lista/Questao_7.c
+++ Questões Lista/Questao_7.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void mdc_diofantino(int a, int b, int *s, int *t);
+int mdc_diofantino(int a, int b, int *s, int *t);
 
 int calcular_mdc(int a, int b);
 
@@ -11,20 +11,20 @@ int main() {
 
     //o inverso de a mod b existe se e somente se mdc(a, b) = 1.
     //Seja s o inverso de a mod b. s eh tal que a*s + b*t = 1; (s e t sao inteiros).
-    
-    //verificando se ha inverso (explicacao da funcao na resolucao da questao 5):
-    if (calcular_mdc(a, b) != 1){
-        printf("Nao existe inverso de %d mod %d.\n", a, b);
-        return 0;
-    }
-
-    //se o mdc eh 1, existe inverso, entao o programa continua...
+	
     //definindo os coeficientes s e t:
     int s, t;
 
-    //encontrando o valor de s (explicacao da funcao na resolucao da questao 6):
-    mdc_diofantino(a, b, &s, &t);
-
+    //encontrando o valor de s e o MDC (explicacao da funcao na resolucao da questao 6):
+    int MDC = mdc_diofantino(a, b, &s, &t);
+	
+	//verificando se MDC != 1:
+	if (MDC != 1){
+		printf("Nao ha inverso de %d mod %d.\n", a, b);
+		return 0;
+	}
+	
+	//caso MDC == 1, o programa continua...
     //escolhendo o inverso de a mod b entre 0 e b:
     while (s < 0) s += b;
     if (s >= b) s = s % b;
@@ -42,18 +42,17 @@ int calcular_mdc(int a, int b)
 }
 
 //explicacao na resolucao da questao 6
-//transformando a funcao em void, visto que o valor esta sendo obtido com ponteiros.
-void mdc_diofantino(int a, int b, int *s, int *t) {
+int mdc_diofantino(int a, int b, int *s, int *t) {
     if(b == 0) {
         *s = 1;
         *t = 0;
-        return;
+        return a;
     }
   
     int s1, t1;
   
-    mdc_diofantino(b, a % b, &s1, &t1);
+    int MDC = mdc_diofantino(b, a % b, &s1, &t1);
     *s = t1;
     *t = s1 - t1*(a/b);
-    return;
+    return MDC;
 }
