5ad91d3650791ebe7c006df6d46a9589ae88b9a1
VinnieT1 <92376175+VinnieT1@users.noreply.github.com>
2021-12-07 22:29:31 -0300

    Letra a) comentada. (1ª questão)
    comentando os passos

--- Questões Lista/a.c
+++ Questões Lista/a.c
@@ -5,18 +5,23 @@
 bool eh_primo(int num){
     if (num <= 1) return false;
     if (num == 2) return true;
-
+    
+    //checando se há algum numero que divide num, tal que este numero seja diferente de 1 e diferente de num.
     for(int i = 2; i*i <= num; i++){
+        //se tal numero for achado, num nao eh primo, logo, retorna false.
         if (num % i == 0) return false;
     }
-
+    
+    //caso nao haja divisor diferente de 1 e de num, num eh primo e retorna true.
     return true;
 }
 
 int main(){
+    //numero eh a variavel que julgaremos ser primo ou nao.
     int numero;
     scanf("%d", &numero);
-
+    
+    //printa se numero eh primo ou nao.
     if (eh_primo(numero)) printf("%d eh primo!\n", numero);
     else printf("%d nao eh primo!\n", numero);
 
